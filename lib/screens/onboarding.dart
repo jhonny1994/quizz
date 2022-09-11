b4febe648caad55c661449004bd6525a3ff02fa1
mohamed abderraouf douadi <me.is.intik@gmail.com>
2022-09-11 16:04:40 +0100

    added checking for internet connection

--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:quizz/constants.dart';
 import 'package:quizz/screens/quiz/choosing_difficulty.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -54,21 +55,37 @@ class _OnboardingState extends State<Onboarding> {
               width: MediaQuery.of(context).size.width * 0.75,
             ),
             const Spacer(),
-            InkWell(
-              customBorder: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              onTap: () => Navigator.of(context).pushAndRemoveUntil(
-                  MaterialPageRoute(
-                    builder: (context) => const ChoosingDifficulty(),
-                  ),
-                  (route) => false),
-              child: const Icon(
-                size: 64,
-                Icons.arrow_circle_right,
-                color: primaryColor,
-              ),
-            ),
+            StreamBuilder(
+                stream: InternetConnectionChecker().onStatusChange,
+                builder: (context, snapshot) {
+                  final isDeviceConnected = snapshot.data;
+                  if (isDeviceConnected == InternetConnectionStatus.connected) {
+                    return IconButton(
+                      iconSize: 64,
+                      alignment: Alignment.center,
+                      color: primaryColor,
+                      onPressed: () => Navigator.of(context).pushAndRemoveUntil(
+                          MaterialPageRoute(
+                            builder: (context) => const ChoosingDifficulty(),
+                          ),
+                          (route) => false),
+                      icon: const Icon(
+                        Icons.arrow_circle_right,
+                      ),
+                    );
+                  } else {
+                    return Container(
+                      padding: const EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: primaryColor, width: 2),
+                      ),
+                      child: const Text(
+                        'Check your internet to start the Quizz',
+                        style: TextStyle(color: secondaryColor, fontSize: 16),
+                      ),
+                    );
+                  }
+                }),
             const Spacer(),
           ],
         ),
